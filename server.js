6b244445624a29f65ef96ca22cc755ea6c51e5b9
hannahrajarao <hannahrajarao@gmail.com>
2023-07-11 21:37:47 -0400

    Add post route and function

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
-const notes = require('./db/db.json');
+const fs = require('fs');
+const util = require('util');
+const { nanoid } = require('nanoid/non-secure');
 
 const PORT = 3001;
 const app = express();
@@ -19,8 +21,42 @@ app.get('/api/notes', (req, res) => {
 });
 
 app.post('/api/notes', (req, res) => {
-    res.json(notes);
+  const { title, text } = req.body;
+
+  if(req.body) {
+    const newNote = {
+      title,
+      text,
+      id: nanoid()
+    };
+    readAndAppend(newNote, './db/db.json');
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+  } 
+  else {
+    res.error('Error in adding note');
+  }
 });
+
+
+const readFromFile = util.promisify(fs.readFile);
+
+const readAndAppend = (content, file) => {
+  fs.readFile(file, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      const parsedData = JSON.parse(data);
+      parsedData.push(content);
+      writeToFile(file, parsedData);
+    }
+  });
+}
+
+const writeToFile = (destination, content) => {
+  fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+    err ? console.error(err) : console.info(`\nData written to ${destination}`)
+  )
+};
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
