71efa239327f1a7e2a939e455553b6aa677f4347
hannahrajarao <hannahrajarao@gmail.com>
2023-07-11 21:37:57 -0400

    Change file reading

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('/notes', (req, res) =>
 );
 
 app.get('/api/notes', (req, res) => {
-    res.json(notes);
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
 app.post('/api/notes', (req, res) => {
